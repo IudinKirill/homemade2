0cfbabf03147b68cf30778c957df69234a25615c
IudinKirill <iydinkirill@gmail.com>
2025-12-11 08:52:19 +0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,13 +11,13 @@
         <!-- Кнопки переключения -->
         <div class="flex space-x-4 mb-6">
             <a href="/"
-               class="px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600
+               class="px-6 py-2 bg-blue-500 text-white hover:bg-blue-600
                       {{ request()->is('/') ? 'ring-2 ring-blue-300' : '' }}">
                 Countries
             </a>
 
             <a href="/users"
-               class="px-6 py-2 bg-green-500 text-white rounded hover:bg-green-600
+               class="px-6 py-2 bg-green-500 text-white hover:bg-green-600
                       {{ request()->is('/users') ? 'ring-2 ring-green-300' : '' }}">
                 Users
             </a>
